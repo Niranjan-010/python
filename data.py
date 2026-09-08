ea969db0d14d1bf2a42b788a47ac9ae9a44bc4a9
Niranjan <niranjankhanal44@gmail.com>
2026-09-08 23:52:17 +0545

    done

---
+++ data.py
@@ -0,0 +1,113 @@
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# if a > b:
+#     print(a, "is larger")
+# else:
+#     print(b, "is larger")
+    
+#     num = int(input("Enter a number: "))
+
+# # Normal Python Practice Program
+
+# print("===== PYTHON BASIC PROGRAM =====")
+
+# # Variables
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+
+# print("\nHello", name)
+# print("Your age is:", age)
+
+# # Even or Odd
+# num = int(input("\nEnter a number: "))
+
+# if num % 2 == 0:
+#     print("The number is Even")
+# else:
+#     print("The number is Odd")
+
+# # Positive, Negative or Zero
+# if num > 0:
+#     print("The number is Positive")
+# elif num < 0:
+#     print("The number is Negative")
+# else:
+#     print("The number is Zero")
+
+# # Multiplication Table
+# print("\n===== MULTIPLICATION TABLE =====")
+
+# for i in range(1, 11):
+#     print(num, "x", i, "=", num * i)
+
+# # Sum from 1 to N
+# n = int(input("\nEnter N: "))
+# total = 0
+
+# for i in range(1, n + 1):
+#     total += i
+
+# print("Sum from 1 to", n, "=", total)
+
+# # Factorial
+# fact = 1
+
+# for i in range(1, n + 1):
+#     fact *= i
+
+# print("Factorial of", n, "=", fact)
+
+# # Prime Check
+# if n < 2:
+#     print(n, "is Not Prime")
+# else:
+#     prime = True
+
+#     for i in range(2, n):
+#         if n % i == 0:
+#             prime = False
+#             break
+
+#     if prime:
+#         print(n, "is Prime")
+#     else:
+#         print(n, "is Not Prime")
+
+# # List
+# numbers = [10, 20, 30, 40, 50]
+
+# print("\n===== LIST =====")
+# print("List:", numbers)
+# print("First element:", numbers[0])
+# print("Last element:", numbers[-1])
+
+# print("All elements:")
+# for x in numbers:
+#     print(x)
+
+# # Function
+# def add(a, b):
+#     return a + b
+
+# a = int(input("\nEnter first number: "))
+# b = int(input("Enter second number: "))
+
+# print("Addition:", add(a, b))
+
+# # String Reverse
+# text = input("\nEnter a word: ")
+
+# print("Original:", text)
+# print("Reverse:", text[::-1])
+
+# # Count Vowels
+# count = 0
+
+# for ch in text:
+#     if ch in "aeiouAEIOU":
+#         count += 1
+
+# print("Number of vowels:", count)
+
+# print("\n===== PROGRAM FINISHED =====")
